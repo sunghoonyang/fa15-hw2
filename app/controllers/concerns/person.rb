da3dce5e884f3b065969f8572da9b35ee2bc32b2
sunghoonyang <sunghoonyang@github.com>
2015-09-25 12:33:21 -0700

    finished hw2 woot

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,12 +4,12 @@ class Person
   	@myAge = age
   end
   def introduce
-  	self.nickname.to_s  + " " + self.myAge.to_s
+  	@nickname.to_s  + " " + @myAge.to_s
   end
   def birth_year
-  	 2015 - myAge.to_i
+  	 2015 - @myAge.to_i
   end
   def nickname
-  	self.nickname
+  	@nickname
   end
 end
